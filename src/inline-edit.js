210dc67ba583af3b6ac4dba5975983d52af5efa1
Mark <mkay581@gmail.com>
2016-04-29 12:34:24 -0400

    Add onValidate callback which allows intercepting successful committing + Add editingElement to pass entire elements

--- src/inline-edit.js
+++ src/inline-edit.js
@@ -3,6 +3,22 @@ import _ from 'lodash';
 
 
 /**
+ * A function that will be called when the value has changed
+ * @callback {InlineEdit~onChange}
+ * @param {String} newValue - The new value
+ * @param {String} oldValue - The previous value before the change was made
+ * @return {undefined|Boolean} Return false to avoid committing
+ */
+
+/**
+ * A function that will be called before committing user's new value
+ * @callback {InlineEdit~onValidate}
+ * @param {String} newValue - The new value
+ * @param {String} oldValue - The old value before the value was changed
+ * @return {undefined|Boolean} Return false to avoid committing
+ */
+
+/**
  * @class InlineEdit
  * @description Allows you to select a text element on the page and edit it in-place.
  */
@@ -13,43 +29,40 @@ class InlineEdit {
      * @param {HTMLElement} el - The element that is to be made editable
      * @param {Object} [options] - The options
      * @param {String} [options.editingClass] - The CSS class that will be applied to the editable element during editing
-     * @param {String} [options.type] - The element type, defaults to 'text' but also supports "textarea" atm
+     * @param {String} [options.editingElement] - The element representing the editable state
      * @param {Function} [options.onChange] - When the user has committed a new value in the editable field
+     * @param {InlineEdit~onValidate} [options.onValidate] - Triggered right before committing a new value to allow QUICK validation logic
      */
     constructor (el, options) {
 
         this.options = _.extend({
             editingClass: 'editing',
+            editingElement: document.createElement('input'),
             onChange: null,
-            type: 'text'
+            onValidate: null
         }, options);
 
         this.el = el;
+        this._editEl = this.options.editingElement;
 
         this._onClickEventListener = this.onClickElement.bind(this);
-        this._onBlurEventListener = this.onBlurInput.bind(this);
+        this._onBlurEventListener = this.onCommit.bind(this);
 
         this.el.addEventListener('click', this._onClickEventListener, true);
 
-        if (this.options.type === 'textarea') {
-            this._inputEl = document.createElement('textarea');
-        } else {
-            this._inputEl = document.createElement('input');
-            this._inputEl.type = 'text';
-        }
-        this._inputEl.classList.add(this.options.editingClass);
-        this._inputEl.addEventListener('blur', this._onBlurEventListener, true);
+        this._editEl.classList.add(this.options.editingClass);
+        this._editEl.addEventListener('blur', this._onBlurEventListener, true);
     }
 
     showEdit() {
-        if (!this.el.contains(this._inputEl)) {
-            this.el.appendChild(this._inputEl);
+        if (!this.el.contains(this._editEl)) {
+            this.el.appendChild(this._editEl);
         }
         this.editing = true;
 
-        this._inputEl.value = this.el.textContent.trim();
-        this.el.parentNode.replaceChild(this._inputEl, this.el);
-        this._inputEl.focus();
+        this._editEl.value = this.el.textContent.trim();
+        this.el.parentNode.replaceChild(this._editEl, this.el);
+        this._editEl.focus();
     }
 
     hideEdit() {
@@ -58,26 +71,31 @@ class InlineEdit {
             return;
         }
 
-        if (this.el.contains(this._inputEl)) {
-            this.el.removeChild(this._inputEl);
+        if (this.el.contains(this._editEl)) {
+            this.el.removeChild(this._editEl);
         }
-        this._inputEl.parentNode.replaceChild(this.el, this._inputEl);
+        this._editEl.parentNode.replaceChild(this.el, this._editEl);
         this.editing = false;
-        this._inputEl.blur();
+        this._editEl.blur();
     }
 
-    onBlurInput () {
+    onCommit () {
 
         let oldValue = this.el.textContent;
-        this.hideEdit();
 
-        if (this._inputEl.value === this.el.textContent) {
+        if (this._editEl.value === this.el.textContent) {
+            this.hideEdit();
             return;
         }
-        this.el.textContent = this._inputEl.value;
 
+        if (this.options.onValidate && this.options.onValidate(this._editEl.value, oldValue) === false) {
+            return;
+        }
+
+        this.el.textContent = this._editEl.value;
+        this.hideEdit();
         if (this.options.onChange) {
-            this.options.onChange(this._inputEl.value, oldValue);
+            this.options.onChange(this._editEl.value, oldValue);
         }
     }
 
@@ -87,7 +105,7 @@ class InlineEdit {
 
     destroy () {
         this.el.removeEventListener('click', this._onClickEventListener, true);
-        this._inputEl.removeEventListener('blur', this._onBlurEventListener, true);
+        this._editEl.removeEventListener('blur', this._onBlurEventListener, true);
         this.hideEdit();
     }
 
