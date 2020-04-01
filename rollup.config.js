fa7e995532b9d7ffdf0efd5f8eb068ae8b742d89
Mark <mark@heyimmark.com>
2020-04-01 19:53:32 -0400

    Replace uglify with terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
 import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { terser } from "rollup-plugin-terser";
+
+const { ROLLUP_WATCH } = process.env;
 
 export default {
     input: 'src/content-editable.ts',
@@ -8,7 +11,7 @@ export default {
         format: 'esm',
         file: 'dist/content-editable.js'
     },
-    plugins: [resolve(), typescript(), commonjs()],
+    plugins: [resolve(), typescript(), commonjs(), !ROLLUP_WATCH && terser()],
     watch: {
         include: 'src/**'
     }
