491d194cd6df1c0e4ec1944e963bbdb0f30c0712
Mark <mark@heyimmark.com>
2020-04-05 13:30:30 -0400

    Add release-it configuration

---
+++ .release-it.js
@@ -0,0 +1,17 @@
+module.exports = {
+    git: {
+        commitMessage: '${version}',
+        requireCleanWorkingDir: false,
+        requireUpstream: false,
+        tagName: 'v${version}',
+    },
+    github: {
+        release: true,
+        releaseName: '${version}',
+        releaseNotes: null,
+    },
+    hooks: {
+        'before:init': ['npm test'],
+        'after:bump': 'npm run build && npm run banner'
+    },
+};
